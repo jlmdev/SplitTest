84e14c7cc10cbfdd2d68d888c6fb0292a328c311
Josh Mann <jlmann72@gmail.com>
2021-03-09 10:19:58 -0500

    simple array split example

--- Program.cs
+++ Program.cs
@@ -2,11 +2,17 @@
 
 namespace SplitTest
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            string first = "This is a split up sentence.";
+
+            string[] splitArray = first.Split(' ');
+
+            string thisWord = splitArray[0];
+
+            Console.WriteLine(thisWord);
+        }
     }
-  }
 }
